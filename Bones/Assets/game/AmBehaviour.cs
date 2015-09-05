fccd5009db9259623aab65f53d4fded60959d4d5
Andrei Marks <hey@ndrei.us>
2015-09-04 20:45:38 -0500

    controller/stage controller scripts

--- Bones/Assets/game/AmBehaviour.cs
+++ Bones/Assets/game/AmBehaviour.cs
@@ -4,18 +4,9 @@ using System.Collections;
 public class AmBehaviour : MonoBehaviour 
 {
     // Controllers
-    protected CameraController _cameras         { get { return CameraController.Instance; } }
-    protected GameplayController _gameplay      { get { return GameplayController.Instance; } }
-    protected InputController _input            { get { return InputController.Instance; } }
     protected StageController _stage            { get { return StageController.Instance; } }
 
     // Settings
-    protected AnimationSettings _animationSettings{ get { return AnimationSettings.Instance; } }
-    protected CameraSettings _cameraSettings    { get { return CameraSettings.Instance; } }
-    protected InputSettings _inputSettings      { get { return InputSettings.Instance; } }
-    protected GameplaySettings _gameplaySettings{ get { return GameplaySettings.Instance; } }
-    protected PhaseSettings _phaseSettings      { get { return PhaseSettings.Instance; } }
-    protected PrefabSettings _prefabSettings    { get { return PrefabSettings.Instance; } }
 
     // Components
     Transform _t;
@@ -39,7 +30,7 @@ public class AmBehaviour : MonoBehaviour
         { 
             if ( _rb == null )
             {
-                _rb = this.rigidbody;
+                _rb = this.GetComponent<Rigidbody>();
             }
 
             return _rb;
