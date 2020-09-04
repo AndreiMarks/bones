0d628cf232fc7e92306cf2392220ee2aa2f07e4f
Andrei Marks <hey@ndrei.us>
2020-09-04 02:54:11 -0700

    Added Bones.TextWriter namespace + content.

---
+++ Packages/bones/Runtime/TextWriter/TextWriter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.InputSystem;
+
+namespace Bones.TextWriter
+{
+    public class TextWriter
+    {
+        private ITextWriterUI _ui;
+        private ITextWriterSerializer _serializer;
+
+        public TextWriter(ITextWriterUI ui)
+        {
+            _ui = ui;
+
+            Keyboard.current.onTextInput += OnTextInput;
+        }
+
+        private void OnTextInput(char character)
+        {
+            _ui.UpdateWithText(character.ToString());
+        }
+    }
+}
