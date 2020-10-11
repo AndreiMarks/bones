cdf370df2a2a7c50491cab0046eb8bf30435702c
andmarks <hey@ndrei.us>
2020-10-11 11:24:08 -0700

    Replacing Bones with latest work.

---
+++ Runtime/TextWriter/Core/ITextWriterUI.cs
@@ -0,0 +1,9 @@
+﻿namespace Bones.TextWriter
+{
+    public interface ITextWriterUI
+    {
+        void UpdateWithText(string newText);
+        void AppendText(string newText);
+        void DeleteText(int charsToDelte);
+    }
+}
